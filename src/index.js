80eac298628568fa77f3a26e9f61b02b78df775c
Agustin Fiorenza <agustinfiorenza@gmail.com>
2019-02-20 23:02:42 +0100

    Add todos basic structure

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const Rx = require('rx');
+const todosDataSource = require('./todos/datasource');
+
+const todoInput = document.getElementById('todo-input');
+const addTodoButton = document.getElementById('add-todo-button');
+const todosList = document.getElementById('todos-list');
+
+
+const todoInputKeyUpObservable = Rx.Observable.fromEvent(todoInput, 'keyup')
+  .filter(event => event.keyCode === 13)
+  .pluck('target', 'value');
+
+const addTodoButtonObservable = Rx.Observable.fromEvent(addTodoButton, 'click')
+  .map(() => todoInput.value)
+
+todoInputKeyUpObservable
+  .merge(addTodoButtonObservable)
+  .filter(value => value.trim() !== '')
+  .scan((todos, value) => [...todos, value], [])
+  .subscribe((todos) => {
+    todosList.textContent = null;
+
+    todos.map(todo => {
+      const liNode = document.createElement('li');
+
+      liNode.textContent = todo;
+      todosList.appendChild(liNode);
+    });
+
+    todoInput.value = '';
+  })
